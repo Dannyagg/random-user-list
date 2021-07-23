45b2a6d37f03bb0ffe4d88e58aecb64bf2ba7526
Daniel Agbenu <borgadee@gmail.com>
2021-07-23 02:53:08 -0400

    random user list API

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import UserInfo from './components/Usercard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Random User Lookup</h1>
+      <UserInfo />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Usercard.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class UserInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        };
+    }
+
+    componentDidMount() {
+        this._fetchUsercard();
+    }
+
+    _fetchUsercard = async () => {
+        const response = await fetch('https://randomuser.me/api/?results=4').then((response) => response.json());
+        this.setState({
+            users: response.results
+
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <h3>Random Users </h3>
+                {this.state.users.map(users =>
+                    <div class='frame'>
+                        <img alt='user-image' src={users.picture.large}></img> <br />
+
+                        Name: {users.name.title}   {users.name.first}   {users.name.last} <br />
+                        Gender: {users.gender} <br />
+                        Age: {users.dob.age} <br />
+                        Location: {users.location.country}, {users.location.state} <br />
+                        Email: {users.email}
+                       
+
+                    </div>)}
+
+            </>
+        );
+
+    }
+}
+export default UserInfo;
